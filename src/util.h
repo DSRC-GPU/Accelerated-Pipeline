6748c9461b9e66856c60ab03e670fc9a8ffbe045
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-08-07 15:34:48 +0200

    Defined debug print.

--- src/util.h
+++ src/util.h
@@ -5,6 +5,12 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#ifdef DEBUG
+#define DEBUG_PRINT printf
+#else
+#define DEBUG_PRINT(...)
+#endif
+
 #define BLOCK_SIZE 64
 
 void utilVectorSetByScalar(float* dst, float scalar, unsigned int num);
