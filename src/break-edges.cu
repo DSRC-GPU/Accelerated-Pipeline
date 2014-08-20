97cc8c26bead5009d3f159dd109ab1d0f7e673b3
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-08-20 14:42:09 +0200

    Implemented the edge-break kernel.

--- src/break-edges.cu
+++ src/break-edges.cu
@@ -2,10 +2,28 @@
 #include "break-edges.h"
 #include "util.h"
 
+__device__ inline int sgnCmp(float a, float b)
+{
+  return a * b >= 0;
+}
+
 __global__ void breakEdgesKernel(unsigned int numVertices, float* fineValues,
     float* coarseValues, unsigned int* numEdges, unsigned int* edgeTargets)
 {
-
+  unsigned int gid = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  unsigned int numvertices = numEdges[gid];
+  float localValue = fineValues[gid] - coarseValues[gid];
+  for (size_t i = 0; i < numvertices; i++)
+  {
+    unsigned int index = numvertices * i + gid;
+    unsigned int neighbour = edgeTargets[index];
+    float neighbourValue = fineValues[neighbour] - coarseValues[neighbour];
+    if (!sgnCmp(localValue, neighbourValue))
+    {
+      // Removing edge by setting target to itself.
+      edgeTargets[neighbour] = gid;
+    }
+  }
 }
 
 void breakEdges(unsigned int numVertices, float* fineValues,
