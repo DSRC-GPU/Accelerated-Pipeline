5be61cd6b224c828208ca24ce94f60c156952603
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-08-20 15:46:50 +0200

    Setting up kernel frame.

--- src/connected-component.cu
+++ src/connected-component.cu
@@ -1,9 +1,19 @@
 
 #include "connected-component.h"
+#include "util.h"
+
+__global__ void connectedComponentKernel(unsigned int numvertices,
+    unsigned int* numedges, unsigned int* edgeTargets,
+    unsigned int* vertexlabels)
+{
+  // FIXME Implement.
+}
 
 void connectedComponent(unsigned int numvertices, unsigned int* numedges,
       unsigned int* edgeTargets, unsigned int* vertexlabels)
 {
-  // FIXME Implement.
+  unsigned int numblocks = ceil(numvertices / (float) BLOCK_SIZE);
+  connectedComponentKernel<<<numblocks, BLOCK_SIZE>>>(numvertices, numedges,
+      edgeTargets, vertexlabels);
 }
 
