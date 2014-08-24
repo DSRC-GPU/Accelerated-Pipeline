4b60f2690eb3ed0108618360e14a9fa1ce82cd66
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-08-24 17:11:34 +0200

    Implemented sequential vector average.

--- src/vector-average.c
+++ src/vector-average.c
@@ -1,32 +1,50 @@
 
+#include "vector-average.h"
+#include "util.h"
+#include <stdlib.h>
+
 float* vectorAverageNewVectorArray(unsigned int numelements)
 {
-
+  return (float*) calloc(2 * numelements, sizeof(float));
 }
 
 void vectorAverageFreeVectorArray(float* averageArray)
 {
-
+  free(averageArray);
 }
 
 float** vectorAverageNewWindow()
 {
-
+  return (float**) calloc(WINDOW_SIZE, sizeof(float*));
 }
 
 void vectorAverageFreeWindow(float** window)
 {
-
+  for (size_t i = 0; i < WINDOW_SIZE; i++)
+  {
+    free(window[i]);
+  }
+  free(window);
 }
 
 void vectorAverageShiftAndAdd(float** window, float* newEntry)
 {
-
+  free(window[0]);
+  for (size_t i = 1; i < WINDOW_SIZE; i++)
+  {
+    window[i - 1] = window[i];
+  }
+  window[WINDOW_SIZE - 1] = newEntry;
 }
 
 void vectorAverageComputeAverage(float** window, unsigned int numelements,
     float* average)
 {
-
+  utilVectorSetByScalar(average, 0, numelements * 2); 
+  for (size_t i = 0; i < WINDOW_SIZE; i++)
+  {
+    utilVectorAdd(average, window[i], numelements * 2);
+  }
+  utilVectorDevideByScalar(average, WINDOW_SIZE, numelements * 2);
 }
 
