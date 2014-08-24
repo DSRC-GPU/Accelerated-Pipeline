7f88a30be065bb331d0ad95b11d5c98afa9aa775
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-08-24 18:05:41 +0200

    Added documentation.

--- src/util.h
+++ src/util.h
@@ -41,6 +41,13 @@ void utilVectorSetByScalar(float* dst, float scalar, unsigned int num);
  */
 void utilVectorAdd(float* dst, float* src, unsigned int num);
 
+/*!
+ * Adds a scalar to a vector.
+ *
+ * \param[in,out] dst The destination vector.
+ * \param[in] scalar The scalar to add.
+ * \param[in] num The number of elements in the vector.
+ */
 void utilVectorAddScalar(float* dst, float scalar, unsigned int num);
 
 /*!
@@ -49,7 +56,7 @@ void utilVectorAddScalar(float* dst, float scalar, unsigned int num);
  * \param[in,out] dst The destination array.
  * \param[in] src The source array.
  * \param[in] num The number of elements in the arrays.
- * \param[in] stream The cuda stream to use.
+ * \param[in] stream_ptr The cuda stream to use.
  */
 void utilVectorAddInStream(float* dst, float* src, unsigned int num,
     void* stream_ptr);
@@ -91,20 +98,66 @@ void utilVectorDevide(float* dst, float* src, unsigned int num);
 void utilVectorDevideByScalar(float* dst, float scalar, unsigned int num);
 
 // TODO Rename function.
+/*!
+ * Sums an array.
+ *
+ * \param[in] d_M The array to sum.
+ * \param[in] numelems The number of elements in the array.
+ * \param[out] d_outVal pointer to where the output should be written.
+ */
 void utilTreeReduction(float* d_M, unsigned int numelems, float* d_outVal);
 
+/*!
+ * Prints an array of floats on the device.
+ *
+ * \param[in] array The array to print.
+ * \param[in] numelems The number of elements to print.
+ */
 void utilPrintDeviceArray(float* array, unsigned int numelems);
 
+/*!
+ * Transfer data from the host to the device. Can free the source of the memory.
+ *
+ * \param[in] src Pointer to the memory that should be moved.
+ * \param[in] numbytes The number of bytes that need to be moved.
+ * \param[in] freeHostMem 1 if the src memory should be freed.
+ * \return Pointer to the allocated memory.
+ */
 void* utilDataTransferHostToDevice(void* src, unsigned int numbytes,
     unsigned int freeHostMem);
 
+/*!
+ * Transfer data from the device to the host. Can free the source of the memory.
+ *
+ * \param[in] src Pointer to the memory that should be moved.
+ * \param[in] numbytes The number of bytes that need to be moved.
+ * \param[in] freeDeviceMem 1 if the src memory should be freed.
+ * \return Pointer to the allocated memory.
+ */
 void* utilDataTransferDeviceToHost(void* src, unsigned int numbytes,
     unsigned int freeDeviceMem);
 
+/*!
+ * Allocates memory on the device.
+ *
+ * \param[in] numbytes The number of bytes to allocate.
+ * \return Pointer to the allocated memory.
+ */
 void* utilAllocateData(unsigned int numbytes);
 
+/*!
+ * Frees device memory.
+ *
+ * \param[in] dptr Pointer to device memory to free.
+ */
 void utilFreeDeviceData(float* dptr);
 
+/*!
+ * Checks and prints cuda errors.
+ *
+ * \param[in] cudaError_t_ptr Pointer to the error struct.
+ * \param[in] msg The message to print if an error is detected.
+ */
 void utilCudaCheckError(void* cudaError_t_ptr, char* msg);
 
 #endif /* UTIL_H_ */
